842d7a0b81d2503b506959801182f28bfc09cae3
Ekrem Iscan <120495676+hammersnow@users.noreply.github.com>
2024-02-23 02:01:58 +0300

    powercycle.sh

--- powercycle.sh
+++ powercycle.sh
@@ -1,77 +1,47 @@
 #!/bin/bash
 #ekremiscan
 
+clear
+echo ""
+echo -e "\033[0;37mPOWERCYCLE KONTROL\033[0m"
+date=$(date +%Y.%m.)
+echo ""
+read -p "Lutfen Son 3 Gun Icindeki Gormek Istediginiz Ayin Gununu Giriniz $date" tarih
 ips=(10.1.10.37 10.1.10.40 10.1.10.35 10.1.10.36 10.1.10.38 10.1.10.39 10.1.10.25 10.1.10.17 10.1.10.23 10.1.10.22 10.1.10.33 10.1.10.32 10.1.10.20 10.1.10.28 10.1.10.21 10.1.10.26 10.1.10.27 10.1.10.24 10.1.10.44 10.1.10.46 10.1.10.43 10.1.10.45 10.1.10.41 10.1.10.42 )
+ttarih=$tarih
 
-
+echo "$ttarih" > /home/eds_cm/eko/tarih
 for ip in "${ips[@]}"; do
+	scp -q /home/eds_cm/eko/tarih $ip:/tmp
         ssh -T $ip << \code 
+scp -q /tmp/tarih scc:/tmp
+rm -f /tmp/tarih
 echo -e "\033[0;37m"
-ipcikti=$(ifconfig | grep addr:10 | awk -F: '{print $2}' | awk '{print $1}')
-if [[ $ipcikti == '10.1.10.37' ]]; then 
-echo "ED01(10.1.10.37)"
-elif [[ $ipcikti == '10.1.10.40' ]]; then 
-echo "ED02(10.1.10.40)"
-elif [[ $ipcikti == '10.1.10.35' ]]; then 
-echo "ED03(10.1.10.35)"
-elif [[ $ipcikti == '10.1.10.36' ]]; then 
-echo "ED04(10.1.10.36)"
-elif [[ $ipcikti == '10.1.10.38' ]]; then 
-echo "ED05(10.1.10.38)"
-elif [[ $ipcikti == '10.1.10.39' ]]; then 
-echo "ED06(10.1.10.39)"
-elif [[ $ipcikti == '10.1.10.25' ]]; then 
-echo "ED09(10.1.10.25)"
-elif [[ $ipcikti == '10.1.10.17' ]]; then 
-echo "ED10(10.1.10.17)"
-elif [[ $ipcikti == '10.1.10.23' ]]; then 
-echo "ED11(10.1.10.23)"
-elif [[ $ipcikti == '10.1.10.22' ]]; then 
-echo "ED12(10.1.10.22)"
-elif [[ $ipcikti == '10.1.10.33' ]]; then 
-echo "ED13(10.1.10.33)"
-elif [[ $ipcikti == '10.1.10.32' ]]; then 
-echo "ED14(10.1.10.32)"
-elif [[ $ipcikti == '10.1.10.20' ]]; then 
-echo "ED15(10.1.10.20)"
-elif [[ $ipcikti == '10.1.10.28' ]]; then 
-echo "ED16(10.1.10.28)"
-elif [[ $ipcikti == '10.1.10.21' ]]; then 
-echo "ED17(10.1.10.21)"
-elif [[ $ipcikti == '10.1.10.26' ]]; then 
-echo "ED18(10.1.10.26)"
-elif [[ $ipcikti == '10.1.10.27' ]]; then 
-echo "ED19(10.1.10.27)"
-elif [[ $ipcikti == '10.1.10.24' ]]; then 
-echo "ED20(10.1.10.24)"
-elif [[ $ipcikti == '10.1.10.44' ]]; then 
-echo "ED23(10.1.10.44)"
-elif [[ $ipcikti == '10.1.10.46' ]]; then 
-echo "ED24(10.1.10.46)"
-elif [[ $ipcikti == '10.1.10.43' ]]; then 
-echo "ED25(10.1.10.43)"
-elif [[ $ipcikti == '10.1.10.45' ]]; then 
-echo "ED26(10.1.10.45)"
-elif [[ $ipcikti == '10.1.10.41' ]]; then 
-echo "ED27(10.1.10.41)"
-elif [[ $ipcikti == '10.1.10.42' ]]; then 
-echo "ED28(10.1.10.42)"
-fi
+edsid=$(cat /opt/eds/etc/scanner_persist.cfg | grep SCANNER | awk '{print $2}')
+echo -e "\033[0;32m$edsid	 		Daily Scc startup and current working time:\033[0m" 
 
 ssh -T scc
+dat=$(date +%b)
+date=$(date +%Y.%m.)
+girdi=$(cat /tmp/tarih)
 last=$(ls -t1 /opt/eds/log/diag/trace/* | head -n 1)
 last2=$(ls -t1 /opt/eds/log/diag/trace/diag* | head -n 2 | tail -n 1)
-if grep -q 'SCS state change: SHUTDOWN => HIBERNATE \| POWER LOSS => SHUTDOWN \| STARTUP => HIBERNATE' $last2 $last;
+last3=$(ls -t1 /opt/eds/log/diag/trace/diag* | head -n 3 | tail -n 1)
+last4=$(ls -t1 /opt/eds/log/diag/trace/diag* | head -n 4 | tail -n 1)
+last5=$(ls -t1 /opt/eds/log/diag/trace/diag* | head -n 5 | tail -n 1)
+if less $last2 $last | grep "SCS state change: SHUTDOWN => HIBERNATE \| POWER LOSS => SHUTDOWN \| HIBERNATE => STARTUP \| STARTUP => HIBERNATE"| grep -q $date$girdi;
 
 then
-        grep "SCS state change: SHUTDOWN => HIBERNATE \| POWER LOSS => SHUTDOWN \| STARTUP => HIBERNATE" $last2 $last | awk -F[ '{print $2}' | awk -F] '{print $1}' | tail -n 1;
-        echo -e "\033[0;32mPowercycle is done.\033[0m"
+        echo -en "\033[0;32mPowercycle is done.\033[0m		"
+	last -x reboot | tac | awk '{print $5, $6, $7, $8, $9, $10, $11}' | tail -n 1
+	grep "SCS state change: SHUTDOWN => HIBERNATE \| POWER LOSS => SHUTDOWN \| STARTUP => HIBERNATE" $last2 $last | awk -F[ '{print $2}' | awk -F] '{print $1}' | grep $date$girdi | tail -n 1;
 	
 else
-        echo -e "\033[0;31mPowercycle is not done!!\033[0m"
-
+        echo -en "\033[0;31mPowercycle is not done!!\033[0m	"
+	last -x reboot | tac | awk '{print $5, $6, $7, $8, $9, $10, $11}' | grep "$dat $girdi" | tail -n 1
 fi
-
+echo ""
+rm -f /tmp/tarih
 
 code
 done
